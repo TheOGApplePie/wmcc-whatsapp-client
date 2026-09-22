26239520321cd880c784cd636005af9e8cd0d01c
Danial Ali <danialali@MacBook-Pro-de-Danial.local>
2026-09-21 22:06:08 -0400

    Replaced explicit chatId with environment variable call

--- src/services/whatsapp.ts
+++ src/services/whatsapp.ts
@@ -1,10 +1,10 @@
 import client from '../whatsapp/client.js';
 import pkg from 'whatsapp-web.js';
-import 'dotenv/config';
 const { MessageMedia } = pkg;
+
 export const sendMessageToWhatsApp = async (message:string, posterUrl:string) => {
   const state = await client.getState();
-  const chatId ='120363408991082605@g.us'// process.env.CHAT_ID
+  const chatId = process.env.CHAT_ID
   if (state !== 'CONNECTED') {
     return {error:"WhatsApp Client not ready"};
   }
@@ -15,10 +15,9 @@ export const sendMessageToWhatsApp = async (message:string, posterUrl:string) =>
   let messageSent;
   if (posterUrl) {
     const messageMedia = await MessageMedia.fromUrl(posterUrl);
-
-    messageSent = await client.sendMessage(chatId, messageMedia, { caption: message });//, {caption:message});
+    messageSent = await client.sendMessage(chatId, messageMedia, { caption: message });
   } else {
-  messageSent =  await client.sendMessage(chatId, message);
+    messageSent =  await client.sendMessage(chatId, message);
   }
   return {status:messageSent }
 }
